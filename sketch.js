efb94a6ee3c85110b10b35e8ca049b5bbad1801f
ianwe19 <ian.welch19@icloud.com>
2024-04-10 18:36:49 -0400

    clean up and comment

--- sketch.js
+++ sketch.js
@@ -12,18 +12,28 @@ function draw() {
     stroke(0, 0, 0); // black stroke
     strokeWeight(w(0.001)); // light stroke weight
 
-    // concentric circles using polygons, so we can deform them
+    // **************************************************************
+    // *  concentric circles using polygons, so we can deform them  *
+    // **************************************************************
+
+    // numSteps = number of vertices in "circle"
     const numSteps = 10;
+
+    // change the type of polygon
     const radiansPerStep = (Math.PI * 2) / numSteps;
+
+    // change last num to make more densely packed polygons, kills performance
     for (let radius = 0.1; radius < 0.4; radius += 0.05) {
-    beginShape();
-    for (let theta = 0; theta < Math.PI * 2; theta += radiansPerStep) {
-        const x = 0.5 + radius * Math.cos(theta);
-        const y = 0.5 + radius * Math.sin(theta);
-        
-        vertex(w(x), h(y));
-    }
-    endShape(CLOSE); // CLOSE because the last point is not the first point
+        beginShape();
+        for (let theta = 0; theta < Math.PI * 2; theta += radiansPerStep) {
+            const x = 0.5 + radius * Math.cos(theta);
+            const y = 0.5 + radius * Math.sin(theta);
+            
+            vertex(w(x), h(y));
+        }
+
+        // CLOSE because the last point is not the first point
+        endShape(CLOSE);
     }
 }
 
