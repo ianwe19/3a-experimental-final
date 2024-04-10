26304d2f3306839c86c220b12bb017d108f839af
ianwe19 <ian.welch19@icloud.com>
2024-04-10 18:20:50 -0400

    initial commit
    new file:   .gitignore
    new file:   index.html
    new file:   jsconfig.json
    new file:   libraries/p5.min.js
    new file:   libraries/p5.sound.min.js
    new file:   p5.min.js
    new file:   p5.sound.min.js
    new file:   sketch.js
    new file:   style.css

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+
+  //             Hue  Sat  Bri  Alpha
+  //              v    v    v    v 
+  colorMode(HSB, 360, 100, 100, 1.0);
+}
+
+function draw() {
+    background(0, 0, 100); // white background
+    noFill(); // no fill
+    stroke(0, 0, 0); // black stroke
+    strokeWeight(w(0.001)); // light stroke weight
+}
+
+// use relative coords for width, 0.0 - 1.0
+function w(val) {
+    if (val == null) return width;
+    return width * val;
+}
+
+
+// use relative coords for height, 0.0 - 1.0
+function h(val) {
+if (val == null) return height;
+return height * val;
+}
