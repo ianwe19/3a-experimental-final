2e3d3e30e6ef8e9f4eeef91f29827f11f59a37bc
ianwe19 <ian.welch19@icloud.com>
2024-04-10 21:43:11 -0400

    animated distorted circles with perlin noise

--- sketch.js
+++ sketch.js
@@ -12,25 +12,18 @@ function draw() {
     stroke(0, 0, 0); // black stroke
     strokeWeight(w(0.001)); // light stroke weight
 
-    // **************************************************************
-    // *  concentric circles using polygons, so we can deform them  *
-    // **************************************************************
+    for (let radius = 0.05; radius < 0.4; radius += 0.05) {
 
-    // numSteps = number of vertices in "circle"
-    const numSteps = 10;
-
-    // change the type of polygon
-    const radiansPerStep = (Math.PI * 2) / numSteps;
-
-    // change last num to make more densely packed polygons, kills performance
-    for (let radius = 0.1; radius < 0.4; radius += 0.05) {
+        // createt array of vertices to make our concentric circles
+        const points = makeCircle(20, radius);
+        
+        // begin drawing path
         beginShape();
-        for (let theta = 0; theta < Math.PI * 2; theta += radiansPerStep) {
-            const x = 0.5 + radius * Math.cos(theta);
-            const y = 0.5 + radius * Math.sin(theta);
-            
-            vertex(w(x), h(y));
-        }
+
+        // iterate through points array, set vertex at each
+        points.forEach(point => {
+          vertex(w(point[0]), h(point[1]));
+        });
 
         // CLOSE because the last point is not the first point
         endShape(CLOSE);
@@ -48,4 +41,18 @@ function w(val) {
 function h(val) {
     if (val == null) return height;
     return height * val;
+}
+
+// make concentric circles using polygons, so we can deform them
+function makeCircle(numSides, radius) {
+    const points = [];
+    const radiansPerStep = (Math.PI * 2) / numSides;
+    for (let theta = 0; theta < Math.PI * 2; theta += radiansPerStep) {
+        const x = 0.5 + radius * Math.cos(theta);
+        const y = 0.5 + radius * Math.sin(theta);
+        
+        points.push([x, y]);
+    }
+
+    return points;
 }
