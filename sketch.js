87f7cbee108b3932badea52347e91588c8dd0230
ianwe19 <ian.welch19@icloud.com>
2024-09-22 18:55:45 -0400

    improve startup text

--- sketch.js
+++ sketch.js
@@ -71,7 +71,7 @@ function draw() {
     else {
         textSize(36);
         fill('white');
-        text("press any key", (windowWidth - 100) / 2, windowHeight / 2);
+        text("press space bar to start, and then move the mouse", (windowWidth - 100) / 2, windowHeight / 2);
         text("\(the text is off center on purpose\)", (windowWidth - 400) / 2, windowHeight / 2 + 50);
     }
     
